cbedce48d5e07fa762ef92a2e640b7a9c6954b98
Nikki Little <nikkilittle13@gmail.com>
2023-09-06 19:24:27 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1 = 31
+let num2 = 2
+function multiply(num1, num2){
+return num1 * num2; 
+}
+var multiply = num1 * num2;
 
-// Write your code here
+
+function random() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+var random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 22
+let num4 = 6
+function mod(num3, num4){
+    return num3 % num4;
+}
+var mod = num3 % num4;
+
+var max = 20;
